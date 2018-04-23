8c411a70f06f8e58fa39f1b741f00f273fe80924
João David <joaodavid1999@hotmail.com>
2018-04-23 09:50:40 +0100

    Changed the matrix to a global variable

--- projeto_IAED.c
+++ projeto_IAED.c
@@ -4,7 +4,7 @@
 
 #define MAXFILENAME 80
 #define MAXELEMENTS 10000
-#define MATRIXLENGHT ULONG_MAX
+#define MAXMLENGHT ULONG_MAX
 
 typedef struct {
     unsigned long line, column;
@@ -12,43 +12,52 @@ typedef struct {
 } MatrixElement;
 
 typedef struct {
-    unsigned long minC, maxC;
     unsigned long minL, maxL;
+    unsigned long minC, maxC;
     /* Dirty flags if the limits are possibly wrong.
     i.e. after deleting an element that is
-    one of the limits.*/
+    part of the limits.*/
     int lenght, dirty;
     double zero;
 } MatrixInfo;
 
-void addElement(MatrixElement matrix[], MatrixInfo *info);
-void deleteEmptySpace(MatrixElement matrix[], int start, int nEls);
-void listRelevant(MatrixElement matrix[], int nEls);
-void printMatrixInfo(MatrixInfo info);
-void printElement(MatrixElement e);
+void addElement();
+void listRelevant();
+void printMatrixInfo();
+void printLine();
+void printColumn();
+void deleteEmptySpace(int start);
+void fixLimits();
+void printElement();
+
+MatrixElement mElements[MAXELEMENTS];
+MatrixInfo mInfo = {MAXMLENGHT, 0, MAXMLENGHT, 0, 0, 0, 0.0};
+
+#define cleanLimits() {mInfo.minL = mInfo.minC = MAXMLENGHT;\
+                       mInfo.maxL = mInfo.maxC = 0;}
+#define isLimit(A) A.line == mInfo.minL || A.line == mInfo.maxL ||\
+                   A.column == mInfo.minC ||  A.column == mInfo.maxC
 
 int main()
 {
     char fileName[MAXFILENAME+1];
-    MatrixInfo info = {MATRIXLENGHT, 0, MATRIXLENGHT, 0, 0, 0, 0.0};
-    MatrixElement elements[MAXELEMENTS];
 
     char command;
-
     do {
         command = getchar();
         /* printf("%d ", command); */
         switch(command) {
             case 'a':
-                addElement(elements, &info);
+                addElement();
                 break;
             case 'p':
-                listRelevant(elements, info.lenght);
+                listRelevant();
                 break;
             case 'i':
-                printMatrixInfo(info);
+                printMatrixInfo();
                 break;
             case 'l':
+                printLine();
                 break;
             case 'c':
                 break;
@@ -72,8 +81,14 @@ int main()
     return 0;
 }
 
-void addElement(MatrixElement matrix[], MatrixInfo *info)
+void addElement()
 {
+    /* Adds a new value to the elements list and also
+    checks if it will redefine the matrix's boundaries.
+    If the value is a zero, it will delete the current
+    value of the position and flag the info as dirty
+    if the element was part of the limits.*/
+
     unsigned long inLine, inColumn;
     double inValue;
     int i;
@@ -82,89 +97,139 @@ void addElement(MatrixElement matrix[], MatrixInfo *info)
 
     /* Check if the position is already occupied. 
     If it is, just change the value) */
-    for (i = 0; i < info->lenght; i++) {
-        if (matrix[i].line == inLine && matrix[i].column == inColumn) {
-            if (inValue != info->zero) {
-                matrix[i].value = inValue;
+    for (i = 0; i < mInfo.lenght; i++) {
+        if (mElements[i].line == inLine && mElements[i].column == inColumn) {
+            if (inValue != mInfo.zero) {
+                mElements[i].value = inValue;
             } else {
-                deleteEmptySpace(matrix, i, info->lenght);
-                /* Check for new max/min line/column. */
-                /* Check for new max/min line/column. */
-                /* Check for new max/min line/column. */
-                /* Check for new max/min line/column. */
-                info->lenght--;
+                if (isLimit(mElements[i]))
+                    mInfo.dirty = 1;
+                
+                deleteEmptySpace(i);
             }
             return;
-        } 
-    } 
+        }
+    }
 
-    if (info->lenght == MAXELEMENTS) {
+    if (mInfo.lenght == MAXELEMENTS) {
         printf("Max Elementes Reached.");
     } else {
-        matrix[info->lenght].line = inLine; 
-        matrix[info->lenght].column = inColumn; 
-        matrix[info->lenght].value = inValue;
+        mElements[mInfo.lenght].line = inLine; 
+        mElements[mInfo.lenght].column = inColumn; 
+        mElements[mInfo.lenght].value = inValue;
 
         /* Check as we add so that we don't iterate 
         over the whole array everytime.*/
-        if (inLine < info->minL)
-            info->minL = inLine;
-        if (inLine > info->maxL)
-            info->maxL = inLine;
-        if (inColumn < info->minC)
-            info->minC = inColumn;
-        if (inColumn > info->maxC)
-            info->maxC = inColumn;
+        if (inLine < mInfo.minL)
+            mInfo.minL = inLine;
+        if (inLine > mInfo.maxL)
+            mInfo.maxL = inLine;
+        if (inColumn < mInfo.minC)
+            mInfo.minC = inColumn;
+        if (inColumn > mInfo.maxC)
+            mInfo.maxC = inColumn;
 
-        info->lenght++;
+        mInfo.lenght++;
     }
 } 
 
-void deleteEmptySpace(MatrixElement matrix[], int start, int nEls)
+void listRelevant()
 {
-    int i;
+    /* Lists every element of the matrix that isn't a zero. */
 
-    for (i = start+1; i < nEls; i++) {
-        matrix[i-1] = matrix[i];
-    }
-}
-
-void listRelevant(MatrixElement matrix[], int nEls)
-{
-    /* Since the zeros are never added or deleted there's
-    no need to check if the element is a zero.*/
+    /* Since the zeros are never added to the list or deleted 
+    there's  no need to check if the element is a zero.*/
 
     int i;
 
-    if (nEls == 0) {
+    if (mInfo.lenght == 0) {
         printf("empty matrix\n");
         return;
     }
 
-    for (i = 0; i < nEls; i++) {
-        printElement(matrix[i]);    
+    for (i = 0; i < mInfo.lenght; i++) {
+        printElement(mElements[i]);    
     }
 }
 
-void printMatrixInfo(MatrixInfo info)
+void printMatrixInfo()
 {
+    /* Prints the "boundaries" of the matrix and
+    its density inside that range.*/ 
+
     int size;
     double dens;
 
-    if (info.lenght == 0) {
+    if (mInfo.lenght == 0) {
         printf("empty matrix\n");
         return;
     }
 
-    size = (info.maxL - info.minL + 1) * (info.maxC - info.minC + 1);
-    dens = ((double) info.lenght/size) * 100;
+    if (mInfo.dirty)
+        fixLimits();
 
-    printf("[%lu %lu] [%lu %lu] %d/%d = %.3f%%\n", info.minL, info.maxL,
-                                                   info.minC, info.maxC,
-                                                   info.lenght, size, dens);    
+    size = (mInfo.maxL - mInfo.minL + 1) * (mInfo.maxC - mInfo.minC + 1);
+    dens = ((double) mInfo.lenght/size) * 100;
+
+    printf("[%lu %lu] [%lu %lu] %d/%d = %.3f%%\n", mInfo.minL, mInfo.maxL,
+                                                   mInfo.minC, mInfo.maxC,
+                                                   mInfo.lenght, size, dens);    
+}
+
+void printLine()
+{
+    unsigned long inLine;
+    int i;
+
+    scanf("%lu", &inLine); 
+
+    for (i = 0; i < mInfo.lenght; i++) {
+        
+    }
+}
+
+void deleteEmptySpace(int start)
+{
+    /* Shifts every element to the left starting from
+    an index.*/
+
+    int i;
+
+    for (i = start+1; i < mInfo.lenght; i++) {
+        mElements[i-1] = mElements[i];
+    }
+
+    mInfo.lenght--;
+}
+
+void fixLimits() 
+{
+    /* Call everytime the limits might be wrong, i.e.
+    the info is flagged as dirty and we need the matrix's
+    boundaries (to print or to calculate a line/column).*/
+
+    int i;
+
+    cleanLimits();
+
+    for (i = 0; i < mInfo.lenght; i++) {
+        printElement(mElements[i]);
+        if (mElements[i].line < mInfo.minL)
+            mInfo.minL = mElements[i].line;
+        if (mElements[i].line > mInfo.maxL)
+            mInfo.maxL = mElements[i].line;
+        if (mElements[i].column < mInfo.minC)
+            mInfo.minC = mElements[i].column;
+        if (mElements[i].column > mInfo.maxC)
+            mInfo.maxC = mElements[i].column;
+    }
+
+    mInfo.dirty = 0;
 }
 
 void printElement(MatrixElement e) 
 {
+    /* Prints the line, column and value of an element. */
+
     printf("[%lu;%lu]=%.3f\n", e.line, e.column, e.value);
 }
